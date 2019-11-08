b6a3456f426d7893028c2e97342818a4079ee460
Gabriel Oliveira <51774020+Gabriel-OSouz@users.noreply.github.com>
2019-11-08 16:43:07 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,13 +1,13 @@
 impot socket
 ip = raw_input('Coloque o IP: ')
-portas = []
+portas = [] # Coloque as portas que deseja analisar
 
 for porta in portas:
     cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     cliente.settime(0,5)
     codigo = cliente.connect_ex((ip,porta))
     if codigo == 0
-        print port, "ABERTO"
+        print porta, "ABERTO"
 
 print "PortScan finalizado com sucesso"
 
